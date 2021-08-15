aae86da8309cd4d1d55902c9d187299e78a9735d
Jayesh Jayanandan <67648301+Jayeshhunter@users.noreply.github.com>
2021-08-15 15:23:48 +0530

    Second commit Jayesh 15|08|2021

--- controllers/formController.js
+++ controllers/formController.js
@@ -83,7 +83,7 @@ module.exports.email_post = async (req, res) => {
       let info = await transporter.sendMail({
         from: process.env.EMAIL, // sender address
         to: email, // list of receivers
-        subject: "Event.io", // Subject line
+        subject: "Recruitment", // Subject line
 
         html: `<!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 1.0 Transitional //EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
           <html xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office">
